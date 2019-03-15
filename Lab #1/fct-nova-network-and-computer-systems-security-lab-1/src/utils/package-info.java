f21825b9a5185d0c4dbffa1d3ee0697140bce1fe
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-03-15 09:51:02 +0000

    Some packages added
    Some packages added

---
+++ Lab #1/fct-nova-network-and-computer-systems-security-lab-1/src/utils/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Network and Computer Systems Security
+ * 
+ * Practical Lab #1.
+ * 
+ * Integrated Master of Computer Science and Engineering
+ * Faculty of Science and Technology of New University of Lisbon
+ * 
+ * Authors (Professors):
+ * @author Henrique Joao Domingos - hj@fct.unl.pt
+ * 
+ * Adapted by:
+ * @author Ruben Andre Barreiro - r.barreiro@campus.fct.unl.pt
+ *
+ */
+
+// The package of Utils.
+package utils;
