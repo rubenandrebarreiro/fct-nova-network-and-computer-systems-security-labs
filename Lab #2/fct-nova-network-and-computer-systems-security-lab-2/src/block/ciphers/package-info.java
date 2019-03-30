831166926b763b4d12050c721cf38033b7becfd0
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-03-30 13:33:29 +0000

    Packages' setup and definition added
    Packages' setup and definition added

---
+++ Lab #2/fct-nova-network-and-computer-systems-security-lab-2/src/block/ciphers/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Network and Computer Systems Security
+ * 
+ * Practical Lab #2.
+ * 
+ * Integrated Master of Computer Science and Engineering
+ * Faculty of Science and Technology of New University of Lisbon
+ * 
+ * Authors (Professors):
+ * @author Henrique Joao Domingos - hj@fct.unl.pt
+ * 
+ * Adapted by:
+ * @author Ruben Andre Barreiro - r.barreiro@campus.fct.unl.pt
+ * @author Vicente Alves Almeida - vo.almeida@campus.fct.unl.pt
+ *
+ */
+
+// The package of Block.Ciphers.
+package block.ciphers;
